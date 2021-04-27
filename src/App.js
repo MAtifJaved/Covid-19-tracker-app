8ca613c7860faf8263bcfc3bfc0ffcfb611606aa
MAtifJaved <atif_javed1992@yahoo.com>
2021-04-27 10:57:17 +0500

    components are added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import Cards from './Components/Cards/Cards';
+import Chart from './Components/Chart/Chart';
+import CountryPicker from './Components/CountryPicker/CountryPicker';
 
-function App() {
-  return (
-    <div>
-      <h1> Hello World</h1>
-      <h2>Hello World 2</h2>
-    </div>
-  );
+
+//import { Cards, CountryPicker, Chart } from './Components';
+import { fetchData } from './api/';
+import styles from './App.module.css';
+
+// import image from './images/image.png';
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        {/* <img className={styles.image} src={image} alt="COVID-19" /> */}
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} /> 
+      </div>
+    );
+  }
 }
 
 export default App;
